140115670e4c6a31669d342a19988f80c4dbc0e8
yusilestari <yusilestari2017@gmail.com>
2022-09-20 16:34:54 +0700

    first commit

---
+++ application/views/tampil.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>latihan pertama</title>
+</head>
+<body>
+    <p>latihan pertama saya belajar fremwork codeighter</p>
+    <p>framwork codeigniter sangat mudah di pelajari</p>
+    
+</body>
+</html>
